9f3db23d5aef580ec57fe93a47562eaf8221162b
dhzdhd <ragas5432100@gmail.com>
2022-03-04 18:59:48 +0530

    Add functionality

--- MediaPlayerRPC/InfoFetcher.fs
+++ MediaPlayerRPC/InfoFetcher.fs
@@ -2,26 +2,36 @@
 
 open System
 open System.Diagnostics
-open Constants
-open Windows.Foundation
+open Windows.Media.Control
 
 type Info =
     { Title: string
       Artist: string
       Album: string }
+    
+type PlayerIsOpen =
+    | Yes of Info
+    | No
 
 module InfoFetcher =
-    let getTrackInfo = 
-        let info = { Title = "Unknown title"; Artist = "Unknown artist"; Album = "Unknown album" }
+    let getTrackInfo () =
+        if not ((Process.GetProcessesByName "Microsoft.Media.Player") |> Array.isEmpty) then
+            let result = GlobalSystemMediaTransportControlsSessionManager.RequestAsync().GetAwaiter().GetResult().GetCurrentSession().TryGetMediaPropertiesAsync().GetAwaiter().GetResult()
+            let info =
+                { Title =
+                      match result.Title with
+                      | "" -> "Unknown Song"
+                      | x -> x
+                  Artist =
+                      match result.AlbumArtist with
+                      | "" -> "Unknown Artist"
+                      | x -> x
+                  Album =
+                      match result.AlbumTitle with
+                      | "" -> "Unknown Album"
+                      | x -> x }
+            printfn $"{info.Album} {info.Artist} {info.Title}"
         
-        let a = (Process.GetProcessesByName "Microsoft.Media.Player") |> Array.isEmpty     
-        printfn "%b" a
-        
-        async {
-            
-        }
-        
-        ()
-    
-    
-
+            Yes info
+        else
+            No
--- MediaPlayerRPC/Program.fs
+++ MediaPlayerRPC/Program.fs
@@ -1,2 +1,57 @@
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+namespace DiscordRPC
+
+open System
+open System.Threading
+open DiscordRPC
+open DiscordRPC.Logging
+open DiscordRPC.Message
+open InfoFetcher
+
+module Main =
+    let onReadyEventHandler (_: ReadyMessage) =
+        printfn "Ready"
+        ()
+        
+    let rec setPresence (client: DiscordRpcClient) =
+        let assets = Assets ()
+        assets.LargeImageKey <- "icon"
+        
+        let button = Button ()
+        button.Label <- "Watch on YouTube"
+        button.Url <- "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        
+        let presence = RichPresence ()
+        presence.Timestamps <- Timestamps (DateTime.Now, Nullable DateTime.UtcNow)
+        presence.Assets <- assets
+        presence.Buttons <- [| button |]
+        
+        
+        match getTrackInfo () with
+        | Yes res ->
+            presence.Details <- res.Title
+            assets.LargeImageText <- $"{res.Album} - {res.Artist}"
+            
+            client.SetPresence presence
+        | No -> ()
+        
+        Async.Sleep 3000 |> Async.RunSynchronously
+        setPresence client
+        
+    let checkInitialized (client: DiscordRpcClient) =
+        match client.IsInitialized with
+        | true -> setPresence client
+        | false -> printfn "Stopped"
+    
+    [<EntryPoint>]
+    let main argv = 
+        let mutable client = new DiscordRpcClient "948624514546270208" 
+        client.Logger <- ConsoleLogger (LogLevel.Warning, true)
+        client.OnReady.Add onReadyEventHandler
+        
+        match client.Initialize () with
+        | true -> printfn "success"
+        | false -> printfn "failure"
+        
+        checkInitialized client
+        
+        0
