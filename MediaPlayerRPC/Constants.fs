5ea1e06320061ae04bf2595ce1e6bc956180f73c
dhzdhd <ragas5432100@gmail.com>
2022-03-04 18:59:07 +0530

    Initial commit

---
+++ MediaPlayerRPC/Constants.fs
@@ -0,0 +1,25 @@
+﻿namespace DiscordRPC
+
+module Constants =
+    let supportedFileTypes = [
+        "aa"
+        "aax"
+        "aac"
+        "aiff"
+        "ape"
+        "dsf"
+        "flac"
+        "m4a"
+        "m4b"
+        "m4p"
+        "mp3"
+        "mpc"
+        "mpp"
+        "ogg"
+        "oga"
+        "wav"
+        "wma"
+        "wv"
+        "webm"
+    ]
+
---
+++ MediaPlayerRPC/InfoFetcher.fs
@@ -0,0 +1,27 @@
+﻿namespace DiscordRPC
+
+open System
+open System.Diagnostics
+open Constants
+open Windows.Foundation
+
+type Info =
+    { Title: string
+      Artist: string
+      Album: string }
+
+module InfoFetcher =
+    let getTrackInfo = 
+        let info = { Title = "Unknown title"; Artist = "Unknown artist"; Album = "Unknown album" }
+        
+        let a = (Process.GetProcessesByName "Microsoft.Media.Player") |> Array.isEmpty     
+        printfn "%b" a
+        
+        async {
+            
+        }
+        
+        ()
+    
+    
+
